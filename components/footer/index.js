c35efde666dd8a15db551c138c514afc04196193
RJ Singh <67853007+rjsiinghhh@users.noreply.github.com>
2022-08-23 19:14:23 -0700

    minor changes

--- components/footer/index.js
+++ components/footer/index.js
@@ -6,8 +6,8 @@ export default function Footer({}) {
  return <div className={styles.footer}>
         <a className={styles.designed} href="https://github.com/rjsiinghhh/portfolio2022">Designed & Developed By RJ Singh</a>
         <div className={styles.icons}>
-       <a href="https://github.com/rjsiinghhh"><i class="fa-brands fa-github-alt"></i></a> 
-        <a href="https://www.instagram.com/rjsiinghhh/"><i class="fa-brands fa-instagram"></i></a>
-       <a href="https://www.linkedin.com/in/arjunsinghh/"><i class="fa-brands fa-linkedin-in"></i></a> 
+       <a href="https://github.com/rjsiinghhh"><i className="fa-brands fa-github-alt"></i></a> 
+        <a href="https://www.instagram.com/rjsiinghhh/"><i className="fa-brands fa-instagram"></i></a>
+       <a href="https://www.linkedin.com/in/arjunsinghh/"><i className="fa-brands fa-linkedin-in"></i></a> 
     </div>
  </div>}
