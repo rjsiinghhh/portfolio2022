c2fc92b71ed062741e0e8eb577d914b02ccb844c
RJ Singh <67853007+rjsiinghhh@users.noreply.github.com>
2023-08-29 15:38:53 -0700

    experience edits

--- components/experience/index.js
+++ components/experience/index.js
@@ -7,26 +7,26 @@ const experiences = [
     {
         jobtitle: "Software Engineer",
         time: "November 2020 - present",
-        descriptions: ["In this role I've worked with SEO agency to create and maintain customer websites using Javascript, HTML, CSS, & Wordpress."],
+        descriptions: ["Marilyn Fricke - In this role I worked with an SEO agency to create, edit and maintain their customer websites. In this role, I worked closely with the team to implement code, troubleshoot technical issues, assist with integrations to enhance SEO competitiveness and ensure that the website is optimized for search. Since 2020, I&apos;ve worked at the agency as a part time engineer and have primarily used HTML, CSS, Javascript, React and Wordpress during this time. "],
         company: "Marilyn Fricke"
     },
     {
         jobtitle: "Software Engineering Fellow",
         time: "August 2020 - November 2020",
-        descriptions: ["Attended General Assembly's award-winning 500-hour Sofware Engineering Immersive program covering full-stack application development integrating object-oriented programming in solo and team building collaborative environments. Focus on Javascript, HTML, CSS, Sass/Scss, PHP, jQuery, React, Node.js, Express, EJS, MongoDB, Mongoose, Postgres, SQL, Bootstrap, Materialize, AJAX, JSON, Figma, WordPress, & Github. Covered in 13 weeks, this experience truly taught me how to continue learning new skills on my own."],
+        descriptions: ["Attended General Assembly's award-winning 500-hour Sofware Engineering Immersive program covering full-stack application development integrating object-oriented programming in solo and team building collaborative environments. Focus on Javascript, HTML, CSS, Sass/Scss, PHP, jQuery, React, Node.js, Express, EJS, MongoDB, Mongoose, Postgres, SQL, Bootstrap, Materialize, AJAX, JSON, Figma, WordPress, & Github. Covered in 13 weeks, this experience gave me the foundation to understand code and to be able to take on more complex tasks on my own."],
         company: "General Assembly"
     },
     {
     jobtitle: "Ranch Manager",
     time: "August 2012 - October 2016 / May 2018 - August 2020",
-    descriptions: ["From 2012 to 2020 I worked with my father in developing and managing our 500 acre orchard estate. This includes pistachios, walnut, and rice. During this time I was ************ give details and explain what I did as. ************* This experience taught me ******************* "],
+    descriptions: ["From 2012 to 2020, I worked with my father in developing and managing our 500 acre orchard estate. The estate included pistachios, walnuts, rice and tomatoes. I worked as the Ranch Manager, overseeing the management of trees, involved in planting and harvesting crops, supervised our team, operated orchard technology and equipment, treated tree & plants for diseases, pests and weeds and set up daily supplies needed for the crew to be successful daily."],
     company: "Pistachio & Walnut Ranch"
     },
     {
     jobtitle: "Time in Coffee & Retail",
-    time: "November 2021- Present",
+    time: "November 2020- Present",
     descriptions: ["In 2016 I took some time from working at the Ranch and moved to SF. I spent a year at Zara, and during this time I worked as a Sale Associate. This experience "],
-    company: "Bluestone Lane, St. Frank Coffee, Zara"
+    company: "Retail & Coffee"
     }
 ]
 
