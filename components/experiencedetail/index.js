19475a813fa63709b75588ac5c47d8c04a1a1416
RJ Singh <67853007+rjsiinghhh@users.noreply.github.com>
2023-09-23 20:31:46 -0700

    updates fonts

--- components/experiencedetail/index.js
+++ components/experiencedetail/index.js
@@ -7,7 +7,7 @@ import styles from "./styles.module.css"
 export default function Experiencedetail({company, jobtitle, time, descriptions}) {
     return <div className={styles.header}>
         <div className={styles.card}>
-            <h3 className={styles.company}>{jobtitle} @ {company}</h3>
+            <h3 className={styles.company}>{jobtitle} | {company}</h3>
             <small className={styles.time + " roboto thin"}>{time}</small>
             {descriptions.map(
                 description => <p key={description} className="roboto thin">{description} </p>
