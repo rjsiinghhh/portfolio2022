88604f8e6394dcf6246a0c0f7319f57ad0bc4dc2
RJ Singh <67853007+rjsiinghhh@users.noreply.github.com>
2023-09-23 19:58:46 -0700

    updates fonts

--- components/home/index.js
+++ components/home/index.js
@@ -17,12 +17,20 @@ export default function Home() {
       <Skills />
      <div id="gallery"></div>
      <Responsivecontainer isMobile={false}>
-      <Gallery projectname="Orgami" description="Coming Soon. The first IOS app I've made, the focus to help people struggling with addictions find help near them." languages={["react native, firebase, crypto-js "]} projectimg={"/zamora.png"} icons={[{icon:"fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/iphoneapp" }]} />
+      <Gallery projectname="Orgami" description="Coming Soon. The first IOS app I've made, the focus to help people struggling with addictions find help near them." languages={["react native, firebase, crypto-js "]} 
+      // projectimg={"/zamora.png"}
+       icons={[{icon:"fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/iphoneapp" }]} />
 
-      <Gallery name="Gallery" projectname="The Zamora Project" description="An online property manager used to track expenses, work notes, and give map details about the real estate." languages={["react", "d3", "express", "postgres", "bootstrap"]} projectimg="/zamora.png" icons={[{ icon: "fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/zamora" }, { icon: "fa-solid fa-share", link:"https://zamoraproject.netlify.app/" }]} reverse />
+      <Gallery name="Gallery" projectname="The Zamora Project" description="An online property manager used to track expenses, work notes, and give map details about the real estate." languages={["react", "d3", "express", "postgres", "bootstrap"]}
+      //  projectimg="/zamora.png" 
+      icons={[{ icon: "fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/zamora" }, { icon: "fa-solid fa-share", link:"https://zamoraproject.netlify.app/" }]} reverse />
+
       <Gallery projectname="Restaurant Finder" description="An application made to search, review, and add your favorite restaurants in any city you choose." languages={["react", "express", "mongodb", "node"]} projectimg="/Restaurantfinder.jpg" icons={[{ icon: "fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/projecthreee" }, { icon: "fa-solid fa-share", link:"https://rjsfoodreviewapp.herokuapp.com/" }]} />
+
       <Gallery projectname="Gowah" description="A real estate listing app that shoes current homes in San Francisco." languages={["react", "php", "postgres"]} projectimg="/gowah.png" icons={[{ icon: "fa-brands fa-github-alt", link: "https://github.com/rjsiinghhh/rj-gowah"}, { icon: "fa-solid fa-share", link:"http://gowahhhomes.herokuapp.com/" }]} reverse />
+
       <Gallery projectname="Coronavirus Update" description="An app where the user can search by country and get live updates on COVID-19 cases." languages={["javascript", "css", "html"]} projectimg="/coronavirusupdate.jpg" icons={[{ icon: "fa-brands fa-github-alt", link:"https://github.com/rjsiinghhh/coronavirus.io" }]}  />
+
       </Responsivecontainer> 
       <Responsivecontainer isMobile>
       <Mobilegallery projectname="The Zamora Project" description="An online property manager used to track expenses, work notes, and give map details about the real estate." languages={["react", "d3", "express", "postgres", "bootstrap"]} projecturl="https://zamoraproject.netlify.app/" projectimg="/zamora.jpg" icons={[{ icon: "fa-brands fa-github-alt" }, { icon: "fa-solid fa-share" }]} link="https://zamoraproject.netlify.app/" reverse />
